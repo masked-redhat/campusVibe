23e9ca17a7d49d0afc672b7d214521543aaea1b4
Zed <skhull.coding@proton.me>
2024-12-22 11:13:42 +0530

    validating username

---
+++ nodejs_backend/utils/username.js
@@ -0,0 +1,21 @@
+function validate(username) {
+  const usernameRegex = /^(?!.*[_.]{2})[a-zA-Z][a-zA-Z0-9._]{2,15}(?<![_.])$/;
+
+  if (usernameRegex.test(username)) {
+    return [true, "Username is valid!"];
+  } else {
+    return [
+      false,
+      "Username is invalid. It must:\n" +
+        "- Be 3 to 16 characters long.\n" +
+        "- Start with a letter.\n" +
+        "- Contain only letters, digits, '_', or '.'.\n" +
+        "- Not have consecutive '_' or '.'.\n" +
+        "- Not start or end with '_' or '.'.",
+    ];
+  }
+}
+
+const username = { validate };
+
+export default username;
