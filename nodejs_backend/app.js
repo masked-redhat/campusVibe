572214e4d195ac3a0ed4f54b854ec9a063b21f54
Zed <skhull.coding@proton.me>
2024-12-27 11:56:28 +0530

    validate -> auth

--- nodejs_backend/app.js
+++ nodejs_backend/app.js
@@ -7,7 +7,7 @@ import Routes from "./routes/router.js";
 import cookieParser from "cookie-parser";
 import authorization from "./middlewares/auth.js";
 const r = Routes,
-  validate = authorization.validateUser;
+  auth = authorization.validateUser;
 
 // Define application params
 const app = express();
@@ -23,20 +23,20 @@ app.use(cookieParser());
 
 // Routes
 app.use("/login", r.LoginRouter);
-app.use("/logout", validate, r.LogoutRouter);
-app.use("/posts", validate, r.PostsRouter);
-app.use("/news", validate, r.NewsRouter);
-app.use("/jobs", validate, r.JobsRouter);
-app.use("/forums", validate, r.ForumsRouter);
-app.use("/comments", validate, r.CommentsRouter);
-app.use("/replies", validate, r.RepliesRouter);
-app.use("/answers", validate, r.AnswersRouter);
-app.use("/feed", validate, r.FeedRouter);
-app.use("/articles", validate, r.ArticlesRouter);
-app.use("/profile", validate, r.ProfileRouter);
-app.use("/feedback", validate, r.FeedbackRouter);
+app.use("/logout", auth, r.LogoutRouter);
+app.use("/posts", auth, r.PostsRouter);
+app.use("/news", auth, r.NewsRouter);
+app.use("/jobs", auth, r.JobsRouter);
+app.use("/forums", auth, r.ForumsRouter);
+app.use("/comments", auth, r.CommentsRouter);
+app.use("/replies", auth, r.RepliesRouter);
+app.use("/answers", auth, r.AnswersRouter);
+app.use("/feed", auth, r.FeedRouter);
+app.use("/articles", auth, r.ArticlesRouter);
+app.use("/profile", auth, r.ProfileRouter);
+app.use("/feedback", auth, r.FeedbackRouter);
 
 // Start the application
 app.listen(port, () => {
-  console.log(`Application at http://localhost:${port}`);
+  console.log(`Application at ${env.url}`);
 });
