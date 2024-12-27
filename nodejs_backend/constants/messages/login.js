5a24d629f106b261a4ac8b54d81741a0425962f5
Zed <skhull.coding@proton.me>
2024-12-27 11:56:08 +0530

    login route messages

---
+++ nodejs_backend/constants/messages/login.js
@@ -0,0 +1,15 @@
+export const MESSAGES = {
+  USERNAME_CONFLICT: "User with that username already exists, choose different",
+  NO_CONTENT: "Email, Password or Username missing",
+  EMAIL_INVALID: "Email is invalid",
+  EMAIL_CONFLICT: "User with same email already exists",
+  CREATED:
+    "User has been created, please verify your email to use the application, if no email has been send, try to go to /email/resend",
+  TOKEN_INVALID: "Email token invalid to verify, try requesting another with /email/resend",
+  USER_NOT_FOUND: "No user found with that username",
+  EMAIL_VERIFIED: "Email verification successfull, you can login now at /login",
+  BAD_PASSWORD: "Password is wrong, please try again",
+  LOGGED_IN: "Log in successfull",
+  EMAIL_UNVERIFIED: "Email is not verified, request for email verification",
+  EMAIL_SENT: "Verification email sent successfully"
+};
