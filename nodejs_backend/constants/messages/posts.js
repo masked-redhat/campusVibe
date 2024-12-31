c2c18f61fc1026043bd9fa162a5df63c6efcd481
Zed <skhull.coding@proton.me>
2024-12-31 09:44:59 +0530

    /post messages

---
+++ nodejs_backend/constants/messages/posts.js
@@ -0,0 +1,17 @@
+export const MESSAGES = {
+  POSTS_FOUND: "Got posts for you",
+    CREATED: "Post created",
+    INVALID_VALUES: "Provided values are not valid",
+    ID_WRONG_NO_ACCESS: "Post id given was wrong, or you are trying to access post you have no access",
+    POST_UPDATED: "Post updated with given data",
+    NO_PROPER_FORMAT: "Fields not in proper format",
+    NO_POST_ID: "No post Id given",
+    COMMENTED: "Commented on this post",
+    COMMENT_UPDATED: "Comment has been updated",
+    NO_COMMENT_ID: "No comment Id given",
+    VOTED: "Voted on this comment",
+    NO_LIKES: "No likes found",
+    ALREADY_LIKED : "You have already liked the post",
+    LIKED: "Liked on this post",
+    UNLIKED: "Unliked this post"
+};
