f2c12b70be683f4ccf7a81d814c8c75b9c9ef8f2
Zed <skhull.coding@proton.me>
2024-12-31 09:32:18 +0530

    Parsing multipart form data and images saving if given in request

--- nodejs_backend/middlewares/parser.js
+++ nodejs_backend/middlewares/parser.js
@@ -1,1 +1,17 @@
-// Parsing Form data
+import multer from "multer";
+import { BACKEND } from "../constants/env.js";
+
+const uploadloc = BACKEND.PUBLIC.LOCATION.IMAGES;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadloc);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export default upload;
