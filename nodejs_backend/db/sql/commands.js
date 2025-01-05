b9f792d5850c97b927a5f109e2333010214decf5
Zed <skhull.coding@proton.me>
2025-01-05 22:12:58 +0530

    now User inclusion will have profile attached

--- nodejs_backend/db/sql/commands.js
+++ nodejs_backend/db/sql/commands.js
@@ -1,9 +1,16 @@
 import User from "../../models/ORM/user.js";
+import Profile from "../../models/ORM/profile.js";
 
 export const userInfoInclusion = {
   model: User,
   foreignKey: "userId",
   attributes: ["username"],
+  include: [
+    {
+      model: Profile,
+      attributes: { exclude: ["id", "userId"] },
+    },
+  ],
 };
 
 export const userInfoByAlias = (alias, foreignKey = "userId") => {
