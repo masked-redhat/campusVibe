503c0811946411ce54e5ba58bbb46f812000e558
Zed <skhull.coding@proton.me>
2025-01-06 14:46:33 +0530

    some function for /forums

--- nodejs_backend/routes/forums/route.js
+++ nodejs_backend/routes/forums/route.js
@@ -1,16 +1,145 @@
 import { Router } from "express";
+import limits from "../../constants/limits.js";
+import checks from "../../utils/checks.js";
+import Forum from "../../models/ORM/forum/forums.js";
+import { userInfoInclusion } from "../../db/sql/commands.js";
+import { simpleOrder } from "../posts/route.js";
+import codes from "../../utils/codes.js";
+import MESSAGES from "../../constants/messages/global.js";
+import { serve } from "../../utils/response.js";
+import User from "../../models/ORM/user.js";
+import transaction from "../../db/sql/transaction.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {});
+const LIMIT = limits.FORUM._;
 
-router.post("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  const { offset: rawOffset, username } = req.query;
+  const offset = checks.isNuldefined(rawOffset) ? 0 : parseInt(rawOffset);
 
-router.patch("/", (req, res) => {});
+  try {
+    const userId = username
+      ? (
+          await User.findOne({
+            attributes: ["id"],
+            where: { username },
+          })
+        )?.id
+      : req.user.id;
 
-router.put("/", (req, res) => {});
+    if (checks.isNuldefined(userId)) {
+      serve(res, codes.BAD_REQUEST, "No user with that username");
+      return;
+    }
 
-router.delete("/", (req, res) => {});
+    const forums = await Forum.findAll({
+      attributes: { exclude: "userId" },
+      where: { userId },
+      offset,
+      limit: LIMIT,
+      order: simpleOrder("updatedAt"),
+      include: [userInfoInclusion],
+    });
+
+    serve(res, codes.OK, "Forums", {
+      forums,
+      offsetNext: offset + forums.length,
+    });
+  } catch (err) {
+    console.log(err);
+
+    serve(res, codes.INTERNAL_SERVER_ERROR, MESSAGES.SERVER_ERROR);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const userId = req.user.id;
+  const { question, content } = req.body;
+
+  const t = await transaction();
+  try {
+    const forum = await Forum.create(
+      { userId, question, content },
+      { attributes: ["id"], transaction: t }
+    );
+
+    await t.commit();
+    serve(res, codes.CREATED, "Forum Created", { forumId: forum.id });
+  } catch (err) {
+    console.log(err);
+    await t.rollback();
+
+    serve(res, codes.INTERNAL_SERVER_ERROR, MESSAGES.SERVER_ERROR);
+  }
+});
+
+router.patch("/", async (req, res) => {
+  const userId = req.user.id;
+  const { question, content, forumId } = req.body;
+
+  const updateData = {
+    ...(question ? { question } : {}),
+    ...(content ? { content } : {}),
+  };
+
+  if (checks.isNuldefined(updateData)) {
+    serve(res, codes.BAD_REQUEST, "Nothing to patch");
+    return;
+  }
+
+  try {
+    await Forum.update(updateData, { where: { userId, id: forumId } });
+    serve(res, codes.OK, "Forum Updated");
+  } catch (err) {
+    console.log(err);
+
+    serve(res, codes.INTERNAL_SERVER_ERROR, MESSAGES.SERVER_ERROR);
+  }
+});
+
+router.put("/", async (req, res) => {
+  const userId = req.user.id;
+  const { question = null, content = null, forumId } = req.body;
+
+  const updateData = { question, content };
+
+  if (checks.isNuldefined(updateData)) {
+    serve(res, codes.BAD_REQUEST, "Nothing to patch");
+    return;
+  }
+
+  try {
+    await Forum.update(updateData, { where: { userId, id: forumId } });
+    serve(res, codes.OK, "Forum Updated");
+  } catch (err) {
+    console.log(err);
+
+    serve(res, codes.INTERNAL_SERVER_ERROR, MESSAGES.SERVER_ERROR);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  const userId = req.user.id;
+  const { forumId } = req.query;
+
+  const t = await transaction();
+  try {
+    await Forum.destroy(
+      { userId, id: forumId },
+      { transaction: t, individualHooks: true }
+    );
+
+    await t.commit();
+
+    serve(res, codes.NO_CONTENT);
+  } catch (err) {
+    console.log(err);
+    await t.rollback();
+
+    serve(res, codes.INTERNAL_SERVER_ERROR, MESSAGES.SERVER_ERROR);
+  }
+});
 
 router.all("*", (_, res) => {
   res.sendStatus(405);
