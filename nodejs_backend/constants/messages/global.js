0a4fc2c3a82db1abb1a568a5d33576999b4ffd3a
Zed <skhull.coding@proton.me>
2025-01-08 01:53:17 +0530

    logout messages and global messages updated

--- nodejs_backend/constants/messages/global.js
+++ nodejs_backend/constants/messages/global.js
@@ -1,6 +1,6 @@
 const MESSAGES = {
   SERVER_ERROR: "Server couldn't handle the request right now, try again later",
-  PARAMETERS_INVALID: "Required parameters not given",
+  PARAMETERS_INVALID: "Required parameters not given or invalid",
 };
 
 export default MESSAGES;
---
+++ nodejs_backend/constants/messages/logout.js
@@ -0,0 +1,3 @@
+export const MESSAGES = {
+  SUCCESS: "Logged out successfully",
+};
