f2f8c88dc80b33bc12565e15d8c4a34465b5dbf5
Zed <skhull.coding@proton.me>
2025-01-08 01:37:47 +0530

    changes the express response by adding different utility functions

--- nodejs_backend/utils/response.js
+++ nodejs_backend/utils/response.js
@@ -1,3 +1,27 @@
-export const serve = (res, statusCode, message = "", args) => {
-  res.status(statusCode).json({ message, ...args });
+import { response } from "express";
+import MESSAGES from "../constants/messages/global.js";
+import codes from "./codes.js";
+
+response.failure = function (statusCode, message = "") {
+  this.status(statusCode).json({ message });
+};
+
+response.ok = function (message = "", additionalData = {}) {
+  this.status(codes.OK).json({ message, ...additionalData });
+};
+
+response.created = function (message = "", additionalData = {}) {
+  this.status(codes.CREATED).json({ message, ...additionalData });
+};
+
+response.serverError = function () {
+  this.failure(codes.INTERNAL_SERVER_ERROR, MESSAGES.SERVER_ERROR);
+};
+
+response.noParams = function () {
+  this.failure(codes.BAD_REQUEST, MESSAGES.PARAMETERS_INVALID);
+};
+
+response.noMethod = function () {
+  this.sendStatus(codes.METHOD_NOT_ALLOWED);
 };
