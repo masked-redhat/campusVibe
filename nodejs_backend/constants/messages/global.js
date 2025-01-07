7e962a9890f4726659c3174095ec1d68b437befd
Zed <skhull.coding@proton.me>
2025-01-08 01:41:10 +0530

    messages has good names and few

--- nodejs_backend/constants/messages/global.js
+++ nodejs_backend/constants/messages/global.js
@@ -1,5 +1,6 @@
 const MESSAGES = {
   SERVER_ERROR: "Server couldn't handle the request right now, try again later",
+  PARAMETERS_INVALID: "Required parameters not given",
 };
 
 export default MESSAGES;
--- nodejs_backend/constants/messages/login.js
+++ nodejs_backend/constants/messages/login.js
@@ -1,19 +1,11 @@
 export const MESSAGES = {
-  USERNAME_CONFLICT: "User with that username already exists, choose different",
-  NO_CONTENT: "Email, Password or Username missing",
-  EMAIL_INVALID: "Email is invalid",
-  EMAIL_CONFLICT: "User with same email already exists",
-  CREATED:
-    "User has been created, email verification otp has been sent, if no email has been recieved, try to go to /email/resend",
-  TOKEN_INVALID:
-    "Email token invalid to verify, try requesting another with /email/resend",
-  USER_NOT_FOUND: "No user found with that username",
-  EMAIL_VERIFIED: "Email verification successfull, you can login now at /login",
-  BAD_PASSWORD: "Password is wrong, please try again",
-  LOGGED_IN: "Log in successfull",
-  EMAIL_UNVERIFIED: "Email is not verified, request for email verification",
-  EMAIL_SENT: "Verification email sent successfully",
+  USERNAME_INVALID: "No user exists with given username",
+  PASSWORD_WRONG: "Wrong password entered for the user account",
+  EMAIL_UNVERIFIED: "Email is not verified",
   BLACKLISTED: "User blacklisted",
-  OTP_TIMEOUT: "OTP timeout",
-  OTP_INVALID: "OTP invalid",
+  EMAIL_INVALID: "Email is invalid, cannot send verification OTP on it",
+  USERNAME_CONFLICT: "Username is not available, try different",
+  EMAIL_CONFLICT: "This email is already used for different account",
+  CREATED:
+    "User has been created, a verification otp has been sent on the registered email, if no email has been recieved, try to go to /email/resend",
 };
